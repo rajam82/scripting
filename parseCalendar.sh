d1e7df33b86444b6b5e50ddad51d671efb523569
rajam82 <raja.m82@gmail.com>
2017-04-03 15:39:08 +0530

    Add files via upload

---
+++ parseCalendar.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cal 2 1997
+output=$(cal 2 1997);
+
+while read -r line;do
+echo "Printing Each Line";
+echo "$line";
+word=$(echo $line | tr " " "\n");
+set $word;
+echo "Printing Each Day In Row";
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+done<<<"$output";
